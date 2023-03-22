b16065414a14f387e711de3505ee30a7156f3ea1
Brave N <braven230522@gmail.com>
2023-03-22 13:46:22 +0700

    update repo ts

--- src/modules/campaigns/campaigns.repository.ts
+++ src/modules/campaigns/campaigns.repository.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { BaseRepository } from '../../base';
 import { Campaign } from '../../entities/campaigns.entity';
 
 @Injectable()
-export class CampaignsRepository extends Repository<Campaign> {
+export class CampaignsRepository extends BaseRepository<Campaign> {
   constructor(
     @InjectRepository(Campaign)
     repository: Repository<Campaign>,
   ) {
-    super(repository.target, repository.manager, repository.queryRunner);
+    super(repository);
   }
 }
--- src/modules/rules/rules.repository.ts
+++ src/modules/rules/rules.repository.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { BaseRepository } from '../../base';
 import { Rule } from '../../entities/rules.entity';
 
 @Injectable()
-export class RulesRepository extends Repository<Rule> {
+export class RulesRepository extends BaseRepository<Rule> {
   constructor(
     @InjectRepository(Rule)
     repository: Repository<Rule>,
   ) {
-    super(repository.target, repository.manager, repository.queryRunner);
+    super(repository);
   }
 }
