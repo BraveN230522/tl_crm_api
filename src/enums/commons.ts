f7768be54f991a7f5d54d42e49faad42a998ea6c
long <ltlong2311@gmail.com>
2023-03-24 21:37:37 +0700

    add date format

--- src/enums/commons.ts
+++ src/enums/commons.ts
@@ -4,3 +4,12 @@ export enum Role {
   B_MANAGER = 'B_MANAGER',
   ADMIN = 'ADMIN',
 }
+
+export enum DateFormat {
+  Y_M_D = 'year/month/day',
+  D_M_Y = 'day/month/year',
+  D_Y_M = 'day/year/month',
+  M_D_Y = 'month/day/year',
+  M_Y_D = 'month/year/day',
+  Y_D_M = 'year/day/month',
+}
