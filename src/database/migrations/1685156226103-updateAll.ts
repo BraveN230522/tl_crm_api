f53d5b0a66898e9c05a60c415a711a92d70cff7a
long <ltlong2311@gmail.com>
2023-05-27 11:48:51 +0700

    check

---
+++ src/database/migrations/1685156226103-updateAll.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class updateAll1685156226103 implements MigrationInterface {
+    name = 'updateAll1685156226103'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        //
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        //    
+    }
+
+}
